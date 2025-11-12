43492ed6330f5e3a9ebd3161e98bdef26b8de773
AbYahaya <yabdulrauf911@gmail.com>
2025-11-12 01:57:25 +0100

    Implemented wallet connect

--- lava_frontend/pages/_app.tsx
+++ lava_frontend/pages/_app.tsx
@@ -4,17 +4,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
+import { MeshProvider } from "@meshsdk/react";
 
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <Component {...pageProps} />
-      </TooltipProvider>
+      <MeshProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <Component {...pageProps} />
+        </TooltipProvider>
+      </MeshProvider>
     </QueryClientProvider>
   );
 }
