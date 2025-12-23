4ed3aedf7be506961345fd2eb688e130abe4bcb4
Samir Idris <samzidris11@gmail.com>
2025-12-23 22:43:03 +0100

    update smart contract to handle ada reward withdrawals, iag staking, and different pools with same assets

--- lava_frontend/src/components/stake/MinTestTokens.tsx
+++ lava_frontend/src/components/stake/MinTestTokens.tsx
@@ -1,78 +1,90 @@
 import { useState } from "react";
-import { Button } from "../ui/button"
+import { Button } from "../ui/button";
 import { toast } from "react-toastify";
 import { useCardanoWallet } from "@/hooks/useCardanoWallet";
 import { mintTestTokens } from "@/e2e/utils/mintTestTokens";
 
 export const MintTestTokens = () => {
   const [isProcessing, setIsProcessing] = useState<boolean>(false);
-  
-  const { txBuilder, blockchainProvider, wallet, walletAddress, walletCollateral, walletUtxos } = useCardanoWallet();
+
+  const {
+    txBuilder,
+    blockchainProvider,
+    wallet,
+    walletAddress,
+    walletCollateral,
+    walletUtxos,
+    reloadWalletState,
+  } = useCardanoWallet();
 
   // Toast
   const toastSuccess = (txHash: string) => {
-    toast.success(<div>
-      Success!  
-      <br />
-      <a
-        href={`https://preprod.cardanoscan.io/transaction/${txHash}`} 
-        target="_blank"
-        rel="noopener noreferrer"
-        style={{ color: "#61dafb", textDecoration: "underline" }}
-      >
-        View on Explorer
-      </a>
-    </div>);
+    toast.success(
+      <div>
+        Success!
+        <br />
+        <a
+          href={`https://preprod.cardanoscan.io/transaction/${txHash}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: "#61dafb", textDecoration: "underline" }}
+        >
+          View on Explorer
+        </a>
+      </div>
+    );
   };
-  const toastFailure = (err: any) => toast.error(`Failed: ${err instanceof Error ? err.message : String(err)}`);
+  const toastFailure = (err: any) =>
+    toast.error(`Failed: ${err instanceof Error ? err.message : String(err)}`);
 
   const handleMintTestTokens = async () => {
-      setIsProcessing(true);
-      console.log("txBuilder:", txBuilder);
-      console.log("blockchainProvider:", blockchainProvider);
+    setIsProcessing(true);
+    console.log("txBuilder:", txBuilder);
+    console.log("blockchainProvider:", blockchainProvider);
 
-      if (!txBuilder || !blockchainProvider || !walletCollateral) {
-        toastFailure("Error: Blockchain provider/txBuilder not initialized");
-        setIsProcessing(false);
-        return;
-      }
-
-      let txHash = "";
-      try {
-        txHash = await mintTestTokens(
-          txBuilder,
-          wallet,
-          walletAddress,
-          walletCollateral,
-          walletUtxos,
-        );
-        txBuilder.reset();
-      } catch (e) {
-        txBuilder.reset();
-        setIsProcessing(false);
-        toastFailure(e);
-        console.error("e tx:", e);
-        console.log("Err in handle mint test tokens");
-        return;
-      }
-  
-      blockchainProvider.onTxConfirmed(txHash, () => {
-        txBuilder.reset();
-        setIsProcessing(false);
-        toastSuccess(txHash);
-        console.log(`Mint test tokens tx hash:`, txHash);
-      });
+    if (!txBuilder || !blockchainProvider || !walletCollateral) {
+      toastFailure("Error: Blockchain provider/txBuilder not initialized");
+      setIsProcessing(false);
+      return;
     }
+
+    let txHash = "";
+    try {
+      txHash = await mintTestTokens(
+        txBuilder,
+        wallet,
+        walletAddress,
+        walletCollateral,
+        walletUtxos
+      );
+      txBuilder.reset();
+    } catch (e) {
+      txBuilder.reset();
+      setIsProcessing(false);
+      toastFailure(e);
+      console.error("e tx:", e);
+      console.log("Err in handle mint test tokens");
+      return;
+    }
+
+    blockchainProvider.onTxConfirmed(txHash, async () => {
+      txBuilder.reset();
+      setIsProcessing(false);
+      toastSuccess(txHash);
+      await reloadWalletState();
+      console.log(`Mint test tokens tx hash:`, txHash);
+    });
+  };
 
   return (
     // <div className="w-full flex justify-center mt-5">
-      <Button
-        disabled={isProcessing}
-        onClick={() => handleMintTestTokens()}
-        className="bg-gradient-lava hover:opacity-90 transition-opacity shadow-glow px-2 py-4"
-      >
-        {isProcessing ? "Processing..." : "Mint Test Tokens"}
-      </Button>
+    <Button
+      disabled={isProcessing}
+      onClick={() => handleMintTestTokens()}
+      className="bg-gradient-lava hover:opacity-90 transition-opacity shadow-glow px-2 py-4"
+    >
+      {isProcessing ? "Processing..." : "Mint Test Tokens"}
+    </Button>
     // </div>
-  )
-}
+  );
+};
--- lava_frontend/src/hooks/useCardanoWallet.ts
+++ lava_frontend/src/hooks/useCardanoWallet.ts
@@ -61,113 +61,112 @@ export function useCardanoWallet() {
   }, [connect, connected]);
 
   // Fetch wallet data (UTxOs and token balances)
-  useEffect(() => {
-    const fetchWalletData = async () => {
-      if (connected && wallet) {
-        try {
-          const addr = await wallet.getChangeAddress();
-          setWalletAddress(addr);
+  const fetchWalletData = async () => {
+    if (connected && wallet) {
+      try {
+        const addr = await wallet.getChangeAddress();
+        setWalletAddress(addr);
 
-          const assets = await wallet.getAssets();
-          const adaAsset = assets.find((a) => a.unit === "lovelace");
-          const balanceInAda = adaAsset
-            ? Number(adaAsset.quantity) / 1_000_000
-            : 0;
-          setBalance(balanceInAda);
+        const assets = await wallet.getAssets();
+        const adaAsset = assets.find((a) => a.unit === "lovelace");
+        const balanceInAda = adaAsset
+          ? Number(adaAsset.quantity) / 1_000_000
+          : 0;
+        setBalance(balanceInAda);
 
-          const { pubKeyHash: walletVK, stakeCredentialHash: walletSK } =
-            deserializeAddress(addr);
-          const walletUtxos = await wallet.getUtxos();
-          // const walletCollateral = (walletUtxos.filter(utxo => (utxo.output.amount.length === 1 && (Number(utxo.output.amount[0].quantity) >= 5000000 && Number(utxo.output.amount[0].quantity) <= 100000000))))[0];
-          const walletCollateral = walletUtxos.filter(
-            (utxo) =>
-              Number(utxo.output.amount[0].quantity) >= 7000000 &&
-              utxo.output.amount.length <= 4
-          )[0];
+        const { pubKeyHash: walletVK, stakeCredentialHash: walletSK } =
+          deserializeAddress(addr);
+        const walletUtxos = await wallet.getUtxos();
+        // const walletCollateral = (walletUtxos.filter(utxo => (utxo.output.amount.length === 1 && (Number(utxo.output.amount[0].quantity) >= 5000000 && Number(utxo.output.amount[0].quantity) <= 100000000))))[0];
+        const walletCollateral = walletUtxos.filter(
+          (utxo) =>
+            Number(utxo.output.amount[0].quantity) >= 7000000 &&
+            utxo.output.amount.length <= 4
+        )[0];
 
-          // Persist the connected wallet
-          if (name) localStorage.setItem(LOCAL_STORAGE_KEY, name);
+        // Persist the connected wallet
+        if (name) localStorage.setItem(LOCAL_STORAGE_KEY, name);
 
-          const maestroKey = process.env.NEXT_PUBLIC_MAESTRO_KEY;
-          if (!maestroKey) {
-            throw new Error("MAESTRO_KEY does not exist");
-          }
-
-          const bp = new MaestroProvider({
-            network: "Preprod",
-            apiKey: maestroKey,
-          });
-          const tb = new MeshTxBuilder({
-            fetcher: bp,
-            submitter: bp,
-            evaluator: bp,
-            verbose: true,
-          });
-          tb.setNetwork("preprod");
-
-          const test = getTokenBalance(
-            assets,
-            "def68337867cb4f1f95b6b811fedbfcdd7780d10a95cc072077088ea",
-            "test"
-          );
-          const stTest = getTokenBalance(
-            assets,
-            "9c1dd9791eba86728634ec4d1531ff3f7ace179c3f8b1e75bfbf1906",
-            "stTest"
-          );
-          const tStrike = getTokenBalance(
-            assets,
-            "def68337867cb4f1f95b6b811fedbfcdd7780d10a95cc072077088ea",
-            "tStrike"
-          );
-          const LStrike = getTokenBalance(
-            assets,
-            "9c1dd9791eba86728634ec4d1531ff3f7ace179c3f8b1e75bfbf1906",
-            "LStrike"
-          );
-          const tPulse = getTokenBalance(
-            assets,
-            "def68337867cb4f1f95b6b811fedbfcdd7780d10a95cc072077088ea",
-            "tPulse"
-          );
-          const LPulse = getTokenBalance(
-            assets,
-            "9c1dd9791eba86728634ec4d1531ff3f7ace179c3f8b1e75bfbf1906",
-            "LPulse"
-          );
-
-          const poolInfoData = await fetchPoolInfo(bp);
-
-          setTxBuilder(tb);
-          setBlockchainProvider(bp);
-          setWalletVK(walletVK);
-          setWalletSK(walletSK);
-          setWalletCollateral(walletCollateral);
-          setWalletUtxos(walletUtxos);
-          setTokenBalances({
-            ...tokenBalances,
-            test,
-            stTest,
-            tStrike,
-            LStrike,
-            tPulse,
-            LPulse,
-          });
-          setPoolInfo(poolInfoData);
-        } catch (err) {
-          console.error("Error fetching wallet data:", err);
+        const maestroKey = process.env.NEXT_PUBLIC_MAESTRO_KEY;
+        if (!maestroKey) {
+          throw new Error("MAESTRO_KEY does not exist");
         }
-      } else {
-        setWalletAddress("");
-        setBalance(0);
-        setWalletUtxos([]);
-        setTokenBalances({});
-        setPoolInfo([]);
-        localStorage.removeItem(LOCAL_STORAGE_KEY);
-        return;
-      }
-    };
 
+        const bp = new MaestroProvider({
+          network: "Preprod",
+          apiKey: maestroKey,
+        });
+        const tb = new MeshTxBuilder({
+          fetcher: bp,
+          submitter: bp,
+          evaluator: bp,
+          verbose: true,
+        });
+        tb.setNetwork("preprod");
+
+        const test = getTokenBalance(
+          assets,
+          "def68337867cb4f1f95b6b811fedbfcdd7780d10a95cc072077088ea",
+          "test"
+        );
+        const stTest = getTokenBalance(
+          assets,
+          "9c1dd9791eba86728634ec4d1531ff3f7ace179c3f8b1e75bfbf1906",
+          "stTest"
+        );
+        const tStrike = getTokenBalance(
+          assets,
+          "def68337867cb4f1f95b6b811fedbfcdd7780d10a95cc072077088ea",
+          "tStrike"
+        );
+        const LStrike = getTokenBalance(
+          assets,
+          "9c1dd9791eba86728634ec4d1531ff3f7ace179c3f8b1e75bfbf1906",
+          "LStrike"
+        );
+        const tPulse = getTokenBalance(
+          assets,
+          "def68337867cb4f1f95b6b811fedbfcdd7780d10a95cc072077088ea",
+          "tPulse"
+        );
+        const LPulse = getTokenBalance(
+          assets,
+          "9c1dd9791eba86728634ec4d1531ff3f7ace179c3f8b1e75bfbf1906",
+          "LPulse"
+        );
+
+        const poolInfoData = await fetchPoolInfo(bp);
+
+        setTxBuilder(tb);
+        setBlockchainProvider(bp);
+        setWalletVK(walletVK);
+        setWalletSK(walletSK);
+        setWalletCollateral(walletCollateral);
+        setWalletUtxos(walletUtxos);
+        setTokenBalances({
+          ...tokenBalances,
+          test,
+          stTest,
+          tStrike,
+          LStrike,
+          tPulse,
+          LPulse,
+        });
+        setPoolInfo(poolInfoData);
+      } catch (err) {
+        console.error("Error fetching wallet data:", err);
+      }
+    } else {
+      setWalletAddress("");
+      setBalance(0);
+      setWalletUtxos([]);
+      setTokenBalances({});
+      setPoolInfo([]);
+      localStorage.removeItem(LOCAL_STORAGE_KEY);
+      return;
+    }
+  };
+  useEffect(() => {
     fetchWalletData();
   }, [connected, wallet, name]);
 
@@ -181,6 +180,10 @@ export function useCardanoWallet() {
     await disconnect();
   };
 
+  const reloadWalletState = async () => {
+    await fetchWalletData();
+  };
+
   return {
     connected,
     wallet,
@@ -190,6 +193,7 @@ export function useCardanoWallet() {
     tokenBalances,
     connect: connectWallet,
     disconnect: disconnectWallet,
+    reloadWalletState,
     blockchainProvider,
     txBuilder,
     walletVK,
