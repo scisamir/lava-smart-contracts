3a4d7e43f2c1d650efdcfa333d3b7d704247f54f
AbYahaya <yabdulrauf911@gmail.com>
2026-01-22 09:36:26 +0100

    still testing

--- lava_frontend/pages/_app.tsx
+++ lava_frontend/pages/_app.tsx
@@ -3,8 +3,7 @@ import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
-//import { Sonner } from "@/components/ui/sonner";
-import { MeshProvider, CardanoWallet } from "@meshsdk/react";  // Add CardanoWallet import
+import { MeshProvider, CardanoWallet } from "@meshsdk/react";
 import { ToastContainer } from "react-toastify";
 
 const queryClient = new QueryClient();
@@ -13,11 +12,12 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <MeshProvider>
+        {/*REQUIRED FOR WALLET PERSISTENCE */}
+        <CardanoWallet />
+
         <TooltipProvider>
-          <ToastContainer position='bottom-left' autoClose={5000} />
+          <ToastContainer position="bottom-left" autoClose={5000} />
           <Toaster />
-          {/* Add CardanoWallet here for persistence across app */}
-          
           <Component {...pageProps} />
         </TooltipProvider>
       </MeshProvider>
