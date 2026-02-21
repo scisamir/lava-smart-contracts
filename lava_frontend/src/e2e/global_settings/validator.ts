838506c9f0c3454a176b2b9146a94fafb3fd5857
Samir Idris <samzidris11@gmail.com>
2026-02-21 07:20:14 +0100

    update frontend to match e2e

--- lava_frontend/src/e2e/global_settings/validator.ts
+++ lava_frontend/src/e2e/global_settings/validator.ts
@@ -1,34 +1,45 @@
-import { applyParamsToScript, builtinByteString, outputReference, resolveScriptHash, serializePlutusScript } from "@meshsdk/core";
+import {
+  applyParamsToScript,
+  builtinByteString,
+  outputReference,
+  resolveScriptHash,
+  serializePlutusScript,
+} from "@meshsdk/core";
 import { setupE2e } from "../setup";
 
 const { blueprint, multisigHash } = setupE2e();
 
-const gsParamTxHash = "a65532a96c8c1ab316f4f6e9bfdf01f04d8b1750f3269ecf74a8e8fe04279bea";
-const gsParamTxIdx = 1;
+const gsParamTxHash =
+  "eacb3b737d595e749e50b5d5e5aa5059dd60eb4c4403d46c1af203819be88913";
+const gsParamTxIdx = 0;
 
-const GlobalSettingsValidator = blueprint.validators.filter(v =>
-    v.title.includes("global_settings.global_settings.spend")
+const GlobalSettingsValidator = blueprint.validators.filter((v) =>
+  v.title.includes("global_settings.global_settings.spend"),
 );
 
 const GlobalSettingsValidatorScript = applyParamsToScript(
-    GlobalSettingsValidator[0].compiledCode,
-    [
-        builtinByteString(multisigHash),
-        outputReference(gsParamTxHash, gsParamTxIdx),
-    ],
-    "JSON"
+  GlobalSettingsValidator[0].compiledCode,
+  [
+    builtinByteString(multisigHash),
+    outputReference(gsParamTxHash, gsParamTxIdx),
+  ],
+  "JSON",
 );
 
-const GlobalSettingsHash = resolveScriptHash(GlobalSettingsValidatorScript, "V3");
+const GlobalSettingsHash = resolveScriptHash(
+  GlobalSettingsValidatorScript,
+  "V3",
+);
 
-const GlobalSettingsAddr = serializePlutusScript(
-    { code: GlobalSettingsValidatorScript, version: "V3" },
-).address;
+const GlobalSettingsAddr = serializePlutusScript({
+  code: GlobalSettingsValidatorScript,
+  version: "V3",
+}).address;
 
 export {
-    GlobalSettingsValidatorScript,
-    GlobalSettingsHash,
-    GlobalSettingsAddr,
-    gsParamTxHash,
-    gsParamTxIdx,
-}
+  GlobalSettingsValidatorScript,
+  GlobalSettingsHash,
+  GlobalSettingsAddr,
+  gsParamTxHash,
+  gsParamTxIdx,
+};
--- lava_frontend/src/e2e/setup.ts
+++ lava_frontend/src/e2e/setup.ts
@@ -1,98 +1,108 @@
 import {
-    NativeScript,
-    applyParamsToScript,
-    resolveNativeScriptHash,
-    resolveScriptHash,
-    serializeNativeScript,
-    stringToHex,
+  NativeScript,
+  applyParamsToScript,
+  resolveNativeScriptHash,
+  resolveScriptHash,
+  serializeNativeScript,
+  stringToHex,
 } from "@meshsdk/core";
 import blueprint from "../../../smart_contract/plutus.json" with { type: "json" };
 
 export const setupE2e = () => {
-    const wallet1VK = "96cbb27c96daf8cab890de6d7f87f5ffd025bf8ac80717cbc4fae7da";
-    const wallet2VK = "331da30f7c8fea429e2bdc161efde817cbb06f78a53ef5ceee42c9a3";
+  const wallet1VK = "1cf3f4f03d7246a72f07b23d3300378f8f2e159716b11ed6f95f73f3";
+  const wallet2VK = "f7dd4a3c0dd50061eaa9b83b9ee1a9ddcd2cf4dc17357940db5a231e";
 
-    // Setup multisig
-    const nativeScript: NativeScript = {
-        type: "all",
-        scripts: [
-            {
-                type: "sig",
-                keyHash: wallet1VK,
-            },
-            {
-                type: "sig",
-                keyHash: wallet2VK,
-            },
-        ],
-    };
-    const { address: multiSigAddress, scriptCbor: multiSigCbor } = serializeNativeScript(nativeScript);
-    // console.log("nativeScript:", nativeScript);
-    // console.log("serializeNativeScript:", serializeNativeScript(nativeScript));
-    const multisigHash = resolveNativeScriptHash(nativeScript);
+  // Setup multisig
+  const nativeScript: NativeScript = {
+    type: "all",
+    scripts: [
+      {
+        type: "sig",
+        keyHash: wallet1VK,
+      },
+      {
+        type: "sig",
+        keyHash: wallet2VK,
+      },
+    ],
+  };
+  const { address: multiSigAddress, scriptCbor: multiSigCbor } =
+    serializeNativeScript(nativeScript);
+  // console.log("nativeScript:", nativeScript);
+  // console.log("serializeNativeScript:", serializeNativeScript(nativeScript));
+  const multisigHash = resolveNativeScriptHash(nativeScript);
 
-    // test mint
-    // Always success mint validator
-    const alwaysSuccessMintValidator = "585401010029800aba2aba1aab9eaab9dab9a4888896600264653001300600198031803800cc0180092225980099b8748000c01cdd500144c9289bae30093008375400516401830060013003375400d149a26cac8009";
-    const alwaysSuccessValidatorMintScript = applyParamsToScript(
-        alwaysSuccessMintValidator,
-        [],
-        "JSON",
-    );
-    const alwaysSuccessMintValidatorHash = resolveScriptHash(alwaysSuccessValidatorMintScript, "V3");
-    console.log("alwaysSuccessMintValidatorHash:", alwaysSuccessMintValidatorHash);
+  // test mint
+  // Always success mint validator
+  const alwaysSuccessMintValidator =
+    "585401010029800aba2aba1aab9eaab9dab9a4888896600264653001300600198031803800cc0180092225980099b8748000c01cdd500144c9289bae30093008375400516401830060013003375400d149a26cac8009";
+  const alwaysSuccessValidatorMintScript = applyParamsToScript(
+    alwaysSuccessMintValidator,
+    [],
+    "JSON",
+  );
+  const alwaysSuccessMintValidatorHash = resolveScriptHash(
+    alwaysSuccessValidatorMintScript,
+    "V3",
+  );
+  console.log(
+    "alwaysSuccessMintValidatorHash:",
+    alwaysSuccessMintValidatorHash,
+  );
 
+  // Constants
+  const GlobalSettingsNft = stringToHex("GSN");
+  const LavaPoolNftName = stringToHex("LPN");
+  const MinPoolLovelace = 5_000_000;
+  const PrecisionFactor = 100_000;
+
+  const testAssetName = stringToHex("test");
+  const testUnit = alwaysSuccessMintValidatorHash + testAssetName;
+  const poolStakeAssetName = stringToHex("stTest");
+
+  const tStrikeAssetName = stringToHex("tStrike");
+  const tStrikeUnit = alwaysSuccessMintValidatorHash + tStrikeAssetName;
+  const tStrikePoolStakeAssetName = stringToHex("LStrike");
+
+  const tPulseAssetName = stringToHex("tPulse");
+  const tPulseUnit = alwaysSuccessMintValidatorHash + tPulseAssetName;
+  const tPulsePoolStakeAssetName = stringToHex("LPulse");
+
+  // Reference scripts
+  const batchingScriptTxHash =
+    "8fdab4023d695d356810bb8ddbadb0afb2043692d68f306ad51e6e0141622a29";
+  const batchingScriptTxIdx = 0;
+  const poolScriptTxHash =
+    "20bd8c588dca842f8a4248f67344d5bba175ea5f7ce82e2531837ec2000dbc4b";
+  const poolScriptTxIdx = 0;
+
+  return {
+    blueprint,
+    wallet1VK,
+    wallet2VK,
+    multisigHash,
+    multiSigAddress,
+    multiSigCbor,
+    alwaysSuccessValidatorMintScript,
+    alwaysSuccessMintValidatorHash,
     // Constants
-    const GlobalSettingsNft = stringToHex("GSN");
-    const LavaPoolNftName = stringToHex("LPN");
-    const MinPoolLovelace = 5_000_000
-    const PrecisionFactor = 100_000;
-
-    const testAssetName = stringToHex("test");
-    const testUnit = alwaysSuccessMintValidatorHash + testAssetName;
-    const poolStakeAssetName = stringToHex("stTest");
-
-    const tStrikeAssetName = stringToHex("tStrike");
-    const tStrikeUnit = alwaysSuccessMintValidatorHash + tStrikeAssetName;
-    const tStrikePoolStakeAssetName = stringToHex("LStrike");
-
-    const tPulseAssetName = stringToHex("tPulse");
-    const tPulseUnit = alwaysSuccessMintValidatorHash + tPulseAssetName;
-    const tPulsePoolStakeAssetName = stringToHex("LPulse");
-
-    // Reference scripts
-    const batchingScriptTxHash = "1bdcbd9d779f426ecf8a4a5e6b2fea600b80998416d4c3ad005db8bb4ac0c1d4";
-    const batchingScriptTxIdx = 0;
-    const poolScriptTxHash = "42fde81fd6f9cc66792b16c6f3934e5084216501f9324d900655a1e209b55296";
-    const poolScriptTxIdx = 0;
-
-    return {
-        blueprint,
-        wallet1VK,
-        wallet2VK,
-        multisigHash,
-        multiSigAddress,
-        multiSigCbor,
-        alwaysSuccessValidatorMintScript,
-        alwaysSuccessMintValidatorHash,
-        // Constants
-        GlobalSettingsNft,
-        LavaPoolNftName,
-        MinPoolLovelace,
-        PrecisionFactor,
-        testAssetName,
-        testUnit,
-        poolStakeAssetName,
-        tStrikeAssetName,
-        tStrikeUnit,
-        tStrikePoolStakeAssetName,
-        tPulseAssetName,
-        tPulseUnit,
-        tPulsePoolStakeAssetName,
-        // Ref scripts
-        batchingScriptTxHash,
-        batchingScriptTxIdx,
-        poolScriptTxHash,
-        poolScriptTxIdx,
-    }
-}
+    GlobalSettingsNft,
+    LavaPoolNftName,
+    MinPoolLovelace,
+    PrecisionFactor,
+    testAssetName,
+    testUnit,
+    poolStakeAssetName,
+    tStrikeAssetName,
+    tStrikeUnit,
+    tStrikePoolStakeAssetName,
+    tPulseAssetName,
+    tPulseUnit,
+    tPulsePoolStakeAssetName,
+    // Ref scripts
+    batchingScriptTxHash,
+    batchingScriptTxIdx,
+    poolScriptTxHash,
+    poolScriptTxIdx,
+  };
+};
